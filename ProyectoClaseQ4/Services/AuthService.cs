57c42e84d7e5ebc09594a490c95454566779db85
Oscarluiis <tinocoscar11@unitec.edu>
2025-11-17 20:52:44 -0600

    login completo

--- ProyectoClaseQ4/Services/AuthService.cs
+++ ProyectoClaseQ4/Services/AuthService.cs
@@ -86,17 +86,72 @@ namespace ProyectoClaseQ4.Services
                 throw new Exception($"Error al registrar usuario: {ex.Message}");
             }
         }
-
+        
+        //Login
         public async Task<AuthResponseDto> Login(LoginDto loginDto)
         {
             try
             {
                 //1. Obtener usuario de Firebase por correo
                 var userCollection = _firebaseService.GetCollection("users");
+                var query = userCollection.WhereEqualTo("Email", loginDto.Email).Limit(1);
+                var snapshot = await query.GetSnapshotAsync();
+
+                if (snapshot.Count == 0)
+                {
+                    throw new Exception("Credenciales invalidas");
+                }
+                var userDoc = snapshot.Documents[0];
+                
+                //2. Extraer campos manualmente
+                var userData = userDoc.ToDictionary();
+                if (userData.ContainsKey("PasswordHash"))
+                {
+                    throw new Exception("Usuario sin contrasenia  configurada");
+                }
+                var passwordHash = userData["PasswordHash"].ToString();
+                
+                //3. Crear el objeto User
+                var user = new User
+                {
+                    Id = userData.ContainsKey("Id") ? userData["Id"].ToString() : userDoc.Id,
+                    Email = userData.ContainsKey("Email").ToString() ?? string.Empty,
+                    Fullname = userData.ContainsKey("FullName").ToString() ?? string.Empty,
+                    Role = userData.ContainsKey("Role") ? userData["Role"].ToString() ?? "user" : "user",
+                    IsActive = userData.ContainsKey("IsActive") && Convert.ToBoolean(userData["IsActive"]),
+                    CreatedAt = userData.ContainsKey("CreatedAt")
+                        ? ((Timestamp)userData["CreatedAt"]).ToDateTime()
+                        : DateTime.UtcNow,
+                };
+                
+                // 4. Verificar contrasenia
+                if (string.IsNullOrEmpty(passwordHash) || !BCrypt.Net.BCrypt.Verify(loginDto.Password, passwordHash))
+                {
+                    throw new Exception("Password invalida");
+                }
+                //5. Verificar que el usuario este activo
+                if (!user.IsActive)
+                {
+                    throw new Exception("Usuario inactivo");
+                }
+                
+                //6. Generar token JWT
+                var token = GenerateJwtToken(user);
+                
+                //7. Retornar respuesta
+                return new AuthResponseDto
+                {
+                    Token = token,
+                    UserId = user.Id,
+                    Email = user.Email,
+                    FullName = user.Fullname,
+                    Role = user.Role
+                };
+
             }
             catch (Exception ex)
             {
-                
+                throw new Exception($"Error al registrar usuario: {ex.Message}");
             }
         }
     }
